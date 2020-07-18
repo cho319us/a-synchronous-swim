4cdd70fb2e9cc6299025c26ce6fd57ff2b915d92
cho319 <cho319us@gmail.com>
2020-07-18 11:52:44 -0700

    Created two tests for background image
    - One for the case that background image does exist
    - One for the case that background image does not exist

--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -17,20 +17,39 @@ module.exports.router = (req, res, next = ()=>{}) => {
   // if request method is equal to OPTIONS
   if (req.method === 'OPTIONS') {
     console.log('Serving request type ' + req.method + ' for url ' + req.url);
-    res.writeHead(200, headers); // send a response statusCode and header
-    res.end(); // assign data to reponse
+    res.writeHead(200, headers); // assign statusCode and header to response
+    res.end(); // asign data to reponse and  signals to the server that all of the response headers and body have been sent
     next(); // invoke next() at the end of a request to help with testing!
   }
   // TODO: why did the tests fail when we used else if?
   // if request method is equal to GET
   if (req.method === 'GET') {
-    console.log('Serving request type ' + req.method + ' for url ' + req.url);
-    // declare commandOptions array
-    // let commandOptions = ['up', 'down', 'left', 'right'];
-    // // get random command
-    // let command = commandOptions[Math.floor(Math.random() * 4)];
-    res.writeHead(200, headers); // send a response statusCode and header
-    res.end(messageQueue.dequeue()); // assign data to reponse
-    next(); // invoke next() at the end of a request to help with testing!
+    // case for command request
+    if (req.url === '/') {
+      console.log('Serving request type ' + req.method + ' for url ' + req.url);
+      // declare commandOptions array
+      // let commandOptions = ['up', 'down', 'left', 'right'];
+      // // get random command
+      // let command = commandOptions[Math.floor(Math.random() * 4)];
+      res.writeHead(200, headers); // assign statusCode and header to response
+      res.end(messageQueue.dequeue()); // asign data to reponse and  signals to the server that all of the response headers and body have been sent
+      next(); // invoke next() at the end of a request to help with testing!
+    // case for background request
+    } else if (req.url === '/background.jpg') {
+      console.log('Serving request type ' + req.method + ' for url ' + req.url);
+      // detect is background image exist
+      fs.readFile(module.exports.backgroundImageFile, (err, data) => {
+        // case for background image does not exist
+        if (err) {
+          res.writeHead(404, headers);
+        // case for background image exist
+        } else {
+          res.writeHead(200, headers);
+          res.write(data); // This sends a chunk of the response body
+        }
+        res.end();
+        next();
+      });
+    }
   }
 };
